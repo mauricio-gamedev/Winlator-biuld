eabc82b3134fbc3bcd77f80851ed48aaa8b6a8e8
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-22 15:40:17 -0400

    test: compile and run RootFS baseline integration checks

--- scripts/test_runtime_integration.sh
+++ scripts/test_runtime_integration.sh
@@ -11,10 +11,13 @@ mkdir -p "$OUT_DIR/classes"
 find "$ROOT_DIR/engine/src/main/java" -name '*.java' -print > "$SOURCES_FILE"
 echo "$ROOT_DIR/app/integration/src/main/java/com/winlator/build/integration/ContainerRuntimeAdapter.java" >> "$SOURCES_FILE"
 echo "$ROOT_DIR/app/integration/src/main/java/com/winlator/build/integration/ContainerRuntimePlanApplier.java" >> "$SOURCES_FILE"
+echo "$ROOT_DIR/app/integration/src/main/java/com/winlator/build/integration/WinlatorRuntimeBaseProbe.java" >> "$SOURCES_FILE"
+echo "$ROOT_DIR/app/integration/src/main/java/com/winlator/build/integration/WinlatorRuntimeInventory.java" >> "$SOURCES_FILE"
 find "$ROOT_DIR/tests/integration/stubs" -name '*.java' -print >> "$SOURCES_FILE"
 find "$ROOT_DIR/tests/integration/java" -name '*.java' -print >> "$SOURCES_FILE"
 
 javac -source 8 -target 8 -d "$OUT_DIR/classes" @"$SOURCES_FILE"
 java -cp "$OUT_DIR/classes" com.winlator.build.integration.ContainerRuntimeAdapterSelfTest
+java -cp "$OUT_DIR/classes" com.winlator.build.integration.WinlatorRuntimeBaseProbeSelfTest
 
 rm -rf "$OUT_DIR"
