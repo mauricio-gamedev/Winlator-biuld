b06cb64d8cef2dd8ade64efa41967a634092e21f
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-22 15:40:34 -0400

    engine: expose runtime base readiness diagnostics

--- engine/src/main/java/com/winlator/build/engine/runtime/RuntimeInventory.java
+++ engine/src/main/java/com/winlator/build/engine/runtime/RuntimeInventory.java
@@ -4,6 +4,11 @@ import com.winlator.build.engine.components.ComponentRegistry;
 
 public interface RuntimeInventory {
     boolean isRuntimeBaseReady();
+
+    default String explainRuntimeBaseUnavailable() {
+        return "runtime base/rootfs is not ready";
+    }
+
     boolean isComponentAvailable(ComponentRegistry.Component component);
     String explainUnavailable(ComponentRegistry.Component component);
 }
