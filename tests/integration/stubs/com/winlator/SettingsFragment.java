446ab40355c6d48839e4bb94ffaccdeb676fe100
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-22 15:51:20 -0400

    test: stub SettingsFragment runtime reset

---
+++ tests/integration/stubs/com/winlator/SettingsFragment.java
@@ -0,0 +1,8 @@
+package com.winlator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public final class SettingsFragment {
+    private SettingsFragment() {}
+    public static void resetPreferenceVersions(AppCompatActivity activity) {}
+}
