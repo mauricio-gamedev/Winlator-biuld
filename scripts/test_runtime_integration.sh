37fc6206a45daa147757c846fec64942773ae4a0
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-22 16:41:54 -0400

    test: run Android Box64 probe integration suite

--- scripts/test_runtime_integration.sh
+++ scripts/test_runtime_integration.sh
@@ -15,6 +15,7 @@ echo "$ROOT_DIR/app/integration/src/main/java/com/winlator/build/integration/Win
 echo "$ROOT_DIR/app/integration/src/main/java/com/winlator/build/integration/WinlatorRuntimeInventory.java" >> "$SOURCES_FILE"
 echo "$ROOT_DIR/app/integration/src/main/java/com/winlator/build/integration/WinlatorRootFsInstaller.java" >> "$SOURCES_FILE"
 echo "$ROOT_DIR/app/integration/src/main/java/com/winlator/build/integration/WinlatorRootFsMaintenanceController.java" >> "$SOURCES_FILE"
+echo "$ROOT_DIR/app/integration/src/main/java/com/winlator/build/integration/WinlatorBox64Probe.java" >> "$SOURCES_FILE"
 find "$ROOT_DIR/tests/integration/stubs" -name '*.java' -print >> "$SOURCES_FILE"
 find "$ROOT_DIR/tests/integration/java" -name '*.java' -print >> "$SOURCES_FILE"
 
@@ -22,5 +23,6 @@ javac -source 8 -target 8 -d "$OUT_DIR/classes" @"$SOURCES_FILE"
 java -cp "$OUT_DIR/classes" com.winlator.build.integration.ContainerRuntimeAdapterSelfTest
 java -cp "$OUT_DIR/classes" com.winlator.build.integration.WinlatorRuntimeBaseProbeSelfTest
 java -cp "$OUT_DIR/classes" com.winlator.build.integration.WinlatorRootFsInstallerSelfTest
+java -cp "$OUT_DIR/classes" com.winlator.build.integration.WinlatorBox64ProbeSelfTest
 
 rm -rf "$OUT_DIR"
