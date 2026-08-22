24f08d6c35c235650a40d7c82546756997eac674
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-22 16:38:13 -0400

    test: run Box64 baseline self-test

--- scripts/test_engine.sh
+++ scripts/test_engine.sh
@@ -14,5 +14,6 @@ find "$ROOT_DIR/engine/src/test/java" -name '*.java' -print >> "$SOURCES_FILE"
 javac -source 8 -target 8 -d "$OUT_DIR/classes" @"$SOURCES_FILE"
 java -cp "$OUT_DIR/classes" com.winlator.build.engine.EngineSelfTest
 java -cp "$OUT_DIR/classes" com.winlator.build.engine.RuntimeReadinessSelfTest
+java -cp "$OUT_DIR/classes" com.winlator.build.engine.runtime.Box64InspectorSelfTest
 
 rm -rf "$OUT_DIR"
