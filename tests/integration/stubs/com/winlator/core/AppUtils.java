13ea0deb57cc662ddabd09bba90b2a695909fde7
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-22 15:57:20 -0400

    test: stub AppUtils for maintenance controller

---
+++ tests/integration/stubs/com/winlator/core/AppUtils.java
@@ -0,0 +1,9 @@
+package com.winlator.core;
+
+import android.content.Context;
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class AppUtils {
+    public static void keepScreenOn(AppCompatActivity activity) {}
+    public static Object showToast(Context context, String text) { return null; }
+}
