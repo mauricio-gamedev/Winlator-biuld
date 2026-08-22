1ee7ebc53a0516d84b1fbc2ea4dab9dbb2e78d31
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-22 15:14:41 -0400

    test: add engine validation script

---
+++ scripts/test_engine.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+set -eu
+
+ROOT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
+OUT_DIR="$ROOT_DIR/.engine-test-out"
+SOURCES_FILE="$OUT_DIR/sources.txt"
+
+rm -rf "$OUT_DIR"
+mkdir -p "$OUT_DIR/classes"
+
+find "$ROOT_DIR/engine/src/main/java" -name '*.java' -print > "$SOURCES_FILE"
+find "$ROOT_DIR/engine/src/test/java" -name '*.java' -print >> "$SOURCES_FILE"
+
+javac -source 8 -target 8 -d "$OUT_DIR/classes" @"$SOURCES_FILE"
+java -cp "$OUT_DIR/classes" com.winlator.build.engine.EngineSelfTest
+
+rm -rf "$OUT_DIR"
