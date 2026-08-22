4b4bf2236229271d50ac18a85218295adbf5e687
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-22 15:57:26 -0400

    test: stub PreloaderDialog for maintenance controller

---
+++ tests/integration/stubs/com/winlator/core/PreloaderDialog.java
@@ -0,0 +1,9 @@
+package com.winlator.core;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PreloaderDialog {
+    public PreloaderDialog(AppCompatActivity activity) {}
+    public void showOnUiThread(int textResId) {}
+    public void closeOnUiThread() {}
+}
