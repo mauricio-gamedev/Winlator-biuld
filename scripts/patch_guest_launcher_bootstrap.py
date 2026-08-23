55419d9ca7bc726748f01d7f9dc8a46700632b52
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-22 22:58:14 -0400

    fix: let Wine perform loader handoff with Box64 path

--- scripts/patch_guest_launcher_bootstrap.py
+++ scripts/patch_guest_launcher_bootstrap.py
@@ -17,10 +17,7 @@ NEW_COMMAND = '''        File loader = new File(rootDir, "/lib/ld-linux-aarch64.
 OLD_ENV = '''        envVars.put("BOX64_LD_LIBRARY_PATH", rootDir+"/lib/x86_64-linux-gnu");'''
 NEW_ENV = '''        envVars.put("BOX64_LD_LIBRARY_PATH", rootDir+"/lib/x86_64-linux-gnu");
         envVars.put("BOX64_PATH", rootDir+rootFS.getWinePath()+"/bin:"+rootDir+"/usr/local/bin:"+rootDir+"/usr/bin");
-        envVars.put("WINELOADERNOEXEC", "1");
-        envVars.put("BOX64_LOG", "2");
-        envVars.put("BOX64_SHOWSEGV", "1");
-        envVars.put("BOX64_DLSYM_ERROR", "1");'''
+        envVars.put("BOX64_LOG", "1");'''
 
 
 def replace_once(text: str, old: str, new: str, label: str) -> str:
