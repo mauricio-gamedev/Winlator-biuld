6e69e5eb1763a75cf6f9fcbd984e1071728b683d
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-22 22:38:36 -0400

    test: cover deep Box64 failure tracing

--- tests/patcher/test_guest_launcher_bootstrap.py
+++ tests/patcher/test_guest_launcher_bootstrap.py
@@ -41,7 +41,9 @@ def main() -> int:
         assert 'terminationCallback.call(-1)' in patched
         assert 'envVars.put("BOX64_PATH", rootDir+rootFS.getWinePath()+"/bin:"+rootDir+"/usr/local/bin:"+rootDir+"/usr/bin")' in patched
         assert 'envVars.put("WINELOADERNOEXEC", "1")' in patched
-        assert 'envVars.put("BOX64_LOG", "1")' in patched
+        assert 'envVars.put("BOX64_LOG", "2")' in patched
+        assert 'envVars.put("BOX64_SHOWSEGV", "1")' in patched
+        assert 'envVars.put("BOX64_DLSYM_ERROR", "1")' in patched
         second = run(path)
         assert second.returncode == 0, second.stderr
         assert path.read_text(encoding="utf-8") == patched
